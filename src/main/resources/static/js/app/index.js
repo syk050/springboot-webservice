aeda384d94555e33b4ee0ffe79ec7de0689e28aa
syk050 <syk050@naver.com>
2022-07-28 17:46:31 +0900

    게시글 등록 버튼 활성화

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,36 @@
+const main = {
+    init: function () {
+        const _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+    },
+    save: function () {
+        const data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(() => {
+            alert('글이 등록되었습니다.');
+            window.location.href= '/';
+        }).fail((error) => {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();
+
+/*
+브라우저의 스코프는 공용 공간으로 쓰이기 떄문에
+나중에 로딩된 js의 init, save가 먼저 로딩된 js의 function을 덮어쓰게 됨
+그렇기 때문에 main이란 객체를 만들어 해당 객체에서 필요한 모든 finction을 선언하는 것
+*/
